87f4f54bd78bc924b2cd6352e47e49d0d93286b8
杨生 <289536143@qq.com>
2018-09-21 10:48:37 +0800

    <dev-1.1.0>springboot集成duboo+mybatis+log4j

--- springboot-iservice-impl/src/main/java/com/springboot/service/impl/IUserServiceImpl.java
+++ springboot-iservice-impl/src/main/java/com/springboot/service/impl/IUserServiceImpl.java
@@ -3,6 +3,7 @@ package com.springboot.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.springboot.dao.UserMapper;
 
+import org.apache.log4j.Logger;
 import org.springboot.beans.vo.User;
 
 import org.springboot.iservice.user.IUserService;
@@ -10,11 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @Service(version = "1.0.0")
 public class IUserServiceImpl implements IUserService {
+	
+	//在使用到的类中声明log以及输出log信息。
+	private static Logger logger=Logger.getLogger(IUserServiceImpl.class);
 	@Autowired
 	private UserMapper userMapper;
 	@Override
-	public User findUser() {	
-		return userMapper.selectUser().get(0);
+	public User findUser() {
+		User user=userMapper.selectUser().get(0);
+		logger.info("core层的日志信息"+user);
+		return user;
 	}
 	@Override
 	public String sayHello(String str) {
--- springboot-web/src/main/java/com/springboot/controller/UserController.java
+++ springboot-web/src/main/java/com/springboot/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.springboot.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 
+import org.apache.log4j.Logger;
 import org.springboot.beans.vo.User;
 import org.springboot.iservice.user.IUserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class UserController {
+	//在使用到的类中声明log以及输出log信息。
+	private static Logger logger=Logger.getLogger(UserController.class);
+	
     @Reference(version = "1.0.0")
     private IUserService iUserService;
 
@@ -20,6 +24,8 @@ public class UserController {
 
     @GetMapping("user")
     public User user() {
-        return iUserService.findUser();
+    	User user=iUserService.findUser();
+    	logger.info("用户的信息"+user);
+        return user;
     }
 }
